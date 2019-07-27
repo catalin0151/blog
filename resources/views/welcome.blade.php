d044210ce82433e6c178a8b6c3ca41efbd20d859
Catalin Ciobanu <ciobanu.m.catalin@gmail.com>
2019-07-27 15:35:10 +0300

    fds
    fsd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel welcome home23sdf</title>
+        <title>Laravel welcome home23sdsdf</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
