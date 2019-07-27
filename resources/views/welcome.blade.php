ca5475712254c84127e0073971c08eccd572ead1
Catalin Ciobanu <ciobanu.m.catalin@gmail.com>
2019-07-27 15:38:51 +0300

    asda

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 a
-        <title>Laravel welcome home2asdsadfsdf3sdsdf</title>
+        <title>Laravel welcome home2asdsadfsdf3jhsdsdf</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
