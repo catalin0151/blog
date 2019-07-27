f101246cfa3d0e925b986b9026ca781f3cb48a95
Catalin Ciobanu <ciobanu.m.catalin@gmail.com>
2019-07-27 16:20:10 +0300

    works

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 a
-        <title>hook wfsdfsfdjhkhjkfork;'kl;k;hl/s;l''1</title>
+        <title>works</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
