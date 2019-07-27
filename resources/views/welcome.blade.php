f3c66c6e868f8bc9d14d182d93dbfaf962c0b29c
Catalin Ciobanu <ciobanu.m.catalin@gmail.com>
2019-07-27 15:38:21 +0300

    dasd
    asd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel welcome home23sdsdf</title>
+a
+        <title>Laravel welcome home2asdsa3sdsdf</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
